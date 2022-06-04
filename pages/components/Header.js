d98e1848649c439812b049fa39afdf108b09dbde
Standpoint <kelivdc@yahoo.com>
2022-06-04 08:29:11 +0700

    Update Stroke

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -22,11 +22,11 @@ function Header() {
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
-            stroke-width="2"
+            strokeWidth="2"
           >
             <path
               strokeLinecap="round"
-              strokeL inejoin="round"
+              strokeLinejoin="round"
               d="M19 9l-7 7-7-7"
             />
           </svg>
