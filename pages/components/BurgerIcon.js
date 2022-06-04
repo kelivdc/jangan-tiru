a08c5ad4ae9cb3294addc7875fa2eaac781b78bd
Standpoint <kelivdc@yahoo.com>
2022-06-04 08:24:51 +0700

    Update

--- pages/components/BurgerIcon.js
+++ pages/components/BurgerIcon.js
@@ -5,15 +5,15 @@ function BurgerIcon() {
     <div className="border p-2 rounded-full visible md:invisible shadow">
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        class="h-6 w-6"
+        className="h-6 w-6"
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
-        stroke-width="2"
+        strokeWidth="2"
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           d="M4 6h16M4 12h16M4 18h16"
         />
       </svg>
--- pages/components/Header.js
+++ pages/components/Header.js
@@ -18,15 +18,15 @@ function Header() {
         <div className="block md:hidden">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
+            className="h-6 w-6"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
             stroke-width="2"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeL inejoin="round"
               d="M19 9l-7 7-7-7"
             />
           </svg>
