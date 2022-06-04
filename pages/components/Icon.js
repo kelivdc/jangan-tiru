f1ad5935463ba332f5f3517f96d3cbdf1eebbb6f
Standpoint <kelivdc@yahoo.com>
2022-06-04 08:34:18 +0700

    Update 3

--- pages/components/Icon.js
+++ pages/components/Icon.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Icon() {
   return (
     <div>
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
     </svg>
     </div>
   )
