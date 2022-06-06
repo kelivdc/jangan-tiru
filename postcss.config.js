1ad2d4f82d0449b363c9c4802cf08484e076545d
Standpoint <kelivdc@yahoo.com>
2022-06-06 19:16:15 +0700

    add PurgeCSS 2

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,37 @@
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//     'postcss-flexbugs-fixes': {},
+//     'postcss-preset-env': {
+//       autoprefixer: {
+//         flexbox: 'no-2009',
+//       },
+//       stage: 3,
+//       features: {
+//         'custom-properties': false,
+//       },
+//     },
+//   },
+// }
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    'postcss-flexbugs-fixes': {},
-    'postcss-preset-env': {
-      autoprefixer: {
-        flexbox: 'no-2009',
-      },
-      stage: 3,
-      features: {
-        'custom-properties': false,
-      },
-    },
-  },
-}
+  plugins: [
+    "tailwindcss",
+    "autoprefixer",
+    [
+      "@fullhuman/postcss-purgecss",
+      process.env.NODE_ENV === "production"
+        ? {
+            // the paths to all template files
+            content: [
+              "./pages/**/*.{js,jsx,ts,tsx}",
+              "./components/**/*.{js,jsx,ts,tsx}",
+            ],
+            // function used to extract class names from the templates
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+          }
+        : false,
+    ],
+  ],
+};
